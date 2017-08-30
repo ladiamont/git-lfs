fed8cba5b4b90251789d1558535cea25bcef3381
Taylor Blau <me@ttaylorr.com>
2017-08-30 15:49:42 -0400

    git/odb/pack: add documentation and tests for 'PackedObjectType'

--- git/odb/pack/type.go
+++ git/odb/pack/type.go
@@ -5,19 +5,33 @@ import (
 	"fmt"
 )
 
+// PackedObjectType is a constant type that is defined for all valid object
+// types that a packed object can represent.
 type PackedObjectType uint8
 
 const (
+	// TypeNone is the zero-value for PackedObjectType, and represents the
+	// absence of a type.
 	TypeNone PackedObjectType = iota
+	// TypeCommit is the PackedObjectType for commit objects.
 	TypeCommit
+	// TypeTree is the PackedObjectType for tree objects.
 	TypeTree
+	// Typeblob is the PackedObjectType for blob objects.
 	TypeBlob
+	// TypeTag is the PackedObjectType for tag objects.
 	TypeTag
 
-	TypeObjectOffsetDelta    PackedObjectType = 6
+	// TypeObjectOffsetDelta is the type for OBJ_OFS_DELTA-typed objects.
+	TypeObjectOffsetDelta PackedObjectType = 6
+	// TypeObjectReferenceDelta is the type for OBJ_REF_DELTA-typed objects.
 	TypeObjectReferenceDelta PackedObjectType = 7
 )
 
+// String implements fmt.Stringer and returns an encoding of the type valid for
+// use in the loose object format protocol (see: package 'git/odb' for more).
+//
+// If the receiving instance is not defined, String() will panic().
 func (t PackedObjectType) String() string {
 	switch t {
 	case TypeNone:
@@ -33,8 +47,9 @@ func (t PackedObjectType) String() string {
 	case TypeObjectOffsetDelta:
 		return "obj_ofs_delta"
 	case TypeObjectReferenceDelta:
-		return "obf_ref_delta"
+		return "obj_ref_delta"
 	}
+
 	panic(fmt.Sprintf("git/odb/pack: unknown object type: %d", t))
 }
 
