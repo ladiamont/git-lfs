f9350bd478150dfc450c8fdda7cec3be55eadcb6
Taylor Blau <me@ttaylorr.com>
2017-08-30 16:50:59 -0400

    git/odb/pack: add tests and documentation to *ChainBase

--- git/odb/pack/chain_base.go
+++ git/odb/pack/chain_base.go
@@ -5,14 +5,25 @@ import (
 	"io"
 )
 
+// ChainBase represents the "base" component of a delta-base chain.
 type ChainBase struct {
+	// offset returns the offset into the given io.ReaderAt where the read
+	// will begin.
 	offset int64
-	size   int64
-	typ    PackedObjectType
+	// size is the total uncompressed size of the data in the base chain.
+	size int64
+	// typ is the type of data that this *ChainBase encodes.
+	typ PackedObjectType
 
+	// r is the io.ReaderAt yielding a stream of zlib-compressed data.
 	r io.ReaderAt
 }
 
+// Unpack inflates and returns the uncompressed data encoded in the base
+// element.
+//
+// If there was any error in reading the compressed data (invalid headers,
+// etc.), it will be returned immediately.
 func (b *ChainBase) Unpack() ([]byte, error) {
 	zr, err := zlib.NewReader(&OffsetReaderAt{
 		r: b.r,
@@ -32,6 +43,7 @@ func (b *ChainBase) Unpack() ([]byte, error) {
 	return buf, nil
 }
 
+// ChainBase returns the type of the object it encodes.
 func (b *ChainBase) Type() PackedObjectType {
 	return b.typ
 }
